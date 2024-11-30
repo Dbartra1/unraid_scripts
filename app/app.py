9446fc196ca63ade83895d28bb3276f6d581f9eb
Dillon Bartram <dfbartram1@gmail.com>
2024-11-29 21:39:21 -0700

    Added the additional app.routes for the other scripts in the repo, changed the html doc to add more script fields.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,11 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, jsonify, render_template
+from app import main as ft
+from Power_off import main as poff
+from Power_on import main as pon
+from Stall_handler import main as stall
+from Overseer_Plex_integration import main as opi
 
 # Load the environment variables from the .env file
 load_dotenv()
@@ -15,11 +20,47 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/api/data", methods=["POST"])
-def handle_data():
-    data = request.json  # Accept JSON input
-    processed_data = f"Processed: {data.get('input')}"  # Example processing
-    return jsonify({"message": "Success", "processed_data": processed_data})
+@app.route("/api/start-transfer", methods=["POST"])
+def start_file_transfer():
+    try:
+        # Trigger the main logic from the file_transfer script
+        ft()
+        return jsonify({"message": "File transfer process initiated successfully."}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/api/plex_power_off", methods=["POST"])
+def plex_power_off():
+    try:
+        poff()
+        return jsonify({"message": "No Plex activity detected, server powered off."}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route("/api/plex_power_on", methods=["POST"])
+def plex_power_on():
+    try:
+        pon()
+        return jsonify({"message": "Plex detected, server powered on."}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route("/api/stall_handler", methods=["POST"])
+def stall_handler():
+    try:
+        stall()
+        return jsonify({"message": "Stalls detected, blocklisting and researching."}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route("/api/overseer_Plex_integration", methods=["POST"])
+def overseer_Plex_integration():
+    try:
+        opi()
+        return jsonify({"message": "Grabbed Plex watchlists and added requests to overseer."}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
 
 if __name__ == "__main__":
     app.run(host=SERVER_IP, port=PORT_NUMBER_FLASK)
